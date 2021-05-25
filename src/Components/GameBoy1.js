b9e07b93d080557d1f4292f4763830098dcdabce
Jordan Molina <jordanmichaelmolina3@gmail.com>
2021-05-25 14:20:48 -0500

    gameboy css (2/2)

--- src/Components/GameBoy1.js
+++ src/Components/GameBoy1.js
@@ -5,20 +5,44 @@ function GameBoy1() {
 		<div>
 			<div className="gameboy__outer__shell">
 				<div className="gameboy__inner__shell">
-					<div class="gameboy__upperhalf">
-						<div class="gameboy__display__wrapper">
+					<div className="gameboy__upperhalf">
+						<div className="gameboy__display__wrapper">
 							&nbsp;
 							<div class="gameboy__display__circle">&nbsp;</div>
 							<div className="gameboy__display__borderwrapper">
-								<div class="gameboy__display">&nbsp;</div>
+								<div className="gameboy__display">&nbsp;</div>
 							</div>
 						</div>
 					</div>
-					<div class="gameboy__lowerhalf">
-						<div class="gameboy__lowerhalf__left">
-							<button class="gameboy__start">Click me</button>
+					<div className="gameboy__lowerhalf">
+						<div className="gameboy__lowerhalf__left">
+							<div className="gameboy__lowerhalf__left__top">
+								<div className="gameboy__buttons">
+									<div className="gameboy__start__wrapper">
+										<button className="gameboy__start"></button>
+										<div className="gameboy__start__borderwrapper">
+											<div className="gameboy__start__label">Start</div>
+										</div>
+									</div>
+									<div className="gameboy__start__wrapper">
+										<button className="gameboy__restart"></button>
+										<div className="gameboy__restart__borderwrapper">
+											<div className="gameboy__restart__label">Restart</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="gameboy__lowerhalf__left__bottom">
+								<div className="gameboy__pause__wrapper">
+									<button className="gameboy__pause"></button>
+									<div className="gameboy__pause__borderwrapper">
+										<div className="gameboy__pause__label">Pause</div>
+									</div>
+								</div>
+							</div>
 						</div>
-						<div class="gameboy__lowerhalf__right">sfsddf</div>
+
+						<div className="gameboy__lowerhalf__right">sfsddf</div>
 					</div>
 				</div>
 			</div>
