fe2ae617ec322d64f9e7ceceb5d2ef01a66d0677
Jordan Molina <jordanmichaelmolina3@gmail.com>
2021-05-25 18:24:08 -0500

    gameboy css and button keyboard functionality

--- src/Components/GameBoy1.js
+++ src/Components/GameBoy1.js
@@ -1,6 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ArrowPress from "../sounds/arrowkey.mp3";
 import "../css/GameBoy1.css";
 function GameBoy1() {
+	const handleKeyDown = (event) => {
+		if (event.key === "ArrowUp") {
+			document.getElementById("uparrow").className =
+				"gameboy__uparrow1__active";
+		} else if (event.key === "ArrowRight") {
+			document.getElementById("rightarrow").className =
+				"gameboy__uparrow1__active";
+		} else if (event.key === "ArrowDown") {
+			document.getElementById("downarrow").className =
+				"gameboy__uparrow1__active";
+		} else if (event.key === "ArrowLeft") {
+			document.getElementById("leftarrow").className =
+				"gameboy__uparrow1__active";
+		}
+	};
+	const handleKeyUp = (event) => {
+		if (event.key === "ArrowUp") {
+			document.getElementById("uparrow").className = "gameboy__uparrow1";
+		} else if (event.key === "ArrowRight") {
+			document.getElementById("rightarrow").className = "gameboy__uparrow1";
+		} else if (event.key === "ArrowDown") {
+			document.getElementById("downarrow").className = "gameboy__uparrow1";
+		} else if (event.key === "ArrowLeft") {
+			document.getElementById("leftarrow").className = "gameboy__uparrow1";
+		}
+	};
+
+	useEffect(() => {
+		document.addEventListener("keydown", handleKeyDown);
+		document.addEventListener("keyup", handleKeyUp);
+		return () => {
+			document.removeEventListener("keydown", handleKeyDown);
+			document.removeEventListener("keyup", handleKeyUp);
+		};
+	}, []);
+
 	return (
 		<div>
 			<div className="gameboy__outer__shell">
@@ -42,7 +79,36 @@ function GameBoy1() {
 							</div>
 						</div>
 
-						<div className="gameboy__lowerhalf__right">sfsddf</div>
+						<div className="gameboy__lowerhalf__right">
+							<div className="gameboy__controls__wrapper">
+								<div className="gameboy__controls__row1">
+									<button id="uparrow" className="gameboy__uparrow1"></button>
+								</div>
+								<div className="gameboy__controls__row2">
+									<button id="leftarrow" className="gameboy__uparrow1"></button>
+									<div className="gameboy__arrows__wrapper">
+										<div className="gameboy__arrows__row1">
+											<button className="gameboy__arrow__up"></button>
+										</div>
+										<div className="gameboy__arrows__row2">
+											<button className="gameboy__arrow__left"></button>
+											&nbsp;
+											<button className="gameboy__arrow__right"></button>
+										</div>
+										<div className="gameboy__arrows__row3">
+											<button className="gameboy__arrow__down"></button>
+										</div>
+									</div>
+									<button
+										id="rightarrow"
+										className="gameboy__uparrow1"
+									></button>
+								</div>
+								<div className="gameboy__controls__row3">
+									<button id="downarrow" className="gameboy__uparrow1"></button>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
