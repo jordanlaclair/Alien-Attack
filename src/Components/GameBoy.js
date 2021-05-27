9ad639f83f839e8b306fee972cc5e3c58bae92f4
Jordan Molina <jordanmichaelmolina3@gmail.com>
2021-05-27 16:59:45 -0500

    started state management using usereducer

--- src/Components/GameBoy.js
+++ src/Components/GameBoy.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useReducer } from "react";
 import ArrowPress from "../sounds/gameboy_arrowkey.mp3";
 import Restart from "../sounds/gameboy_restart.mp3";
 import Start from "../sounds/gameboy_start.mp3";
@@ -33,6 +33,8 @@ function GameBoy() {
 				"gameboy__uparrow1__active";
 		} else if (event.key === "s" || event.key === "S") {
 			start.play();
+			dispatch({ type: "start" });
+
 			document.getElementById("start").className = "gameboy__start__active";
 		} else if (event.key === "r" || event.key === "R") {
 			restart.play();
@@ -68,20 +70,127 @@ function GameBoy() {
 
 	const handleStartButtonPress = (e) => {
 		start.play();
+		dispatch({ type: "start" });
 	};
 	const handleRestartButtonPress = (e) => {
 		restart.play();
 	};
 
+	function loginReducer(state, action) {
+		switch (action.type) {
+			case "start":
+				return {
+					...state,
+					start: true,
+					menu: false,
+					showTimer: true,
+					showTime: true,
+					time: new Date().toLocaleTimeString([], {
+						hour: "2-digit",
+						minute: "2-digit",
+					}),
+				};
+			case "update":
+				return {
+					...state,
+					[action.field]: action.value,
+				};
+
+			default:
+				break;
+		}
+		return state;
+	}
+
+	const initialState = {
+		start: false,
+		restart: false,
+		menu: true,
+		pause: false,
+		time: "",
+		minutes: 0,
+		seconds: 0,
+		score: 0,
+		showTime: false,
+		showTimer: false,
+		totalSeconds: 0,
+	};
+
+	const [state, dispatch] = useReducer(loginReducer, initialState);
+
+	function updateTime() {
+		dispatch({
+			type: "update",
+			field: "totalSeconds",
+			value: (prevValue) => prevValue + 1,
+		});
+		dispatch({
+			type: "update",
+			field: "seconds",
+			value: pad(state.totalSeconds % 60),
+		});
+		dispatch({
+			type: "update",
+			field: "minutes",
+			value: pad(parseInt(state.totalSeconds / 60)),
+		});
+	}
+
+	function pad(val) {
+		var valString = val + "";
+		if (valString.length < 2) {
+			return "0" + valString;
+		} else {
+			return valString;
+		}
+	}
+
 	useEffect(() => {
+		let interval = setInterval(() => {
+			if (state.start) {
+				updateTime();
+
+				dispatch({
+					type: "update",
+					field: "time",
+					value: new Date().toLocaleTimeString([], {
+						hour: "2-digit",
+						minute: "2-digit",
+					}),
+				});
+			}
+			/* dispatch({
+				type: "start",
+				field: "time",
+				value: new Date().toLocaleTimeString([], {
+					hour: "2-digit",
+					minute: "2-digit",
+				}),
+			});  */
+
+			/* dispatch({
+				type: "start",
+				field: "timer",
+				value: new Date().toLocaleTimeString([], {
+					hour: "2-digit",
+					minute: "2-digit",
+				}),
+			}); */
+			//console.log(date.slice(0, date.length - 2));
+		}, 1000);
+
 		document.addEventListener("keydown", handleKeyDown);
 		document.addEventListener("keyup", handleKeyUp);
 		return () => {
 			document.removeEventListener("keydown", handleKeyDown);
 			document.removeEventListener("keyup", handleKeyUp);
+			clearInterval(interval);
 		};
 	}, []);
 
+	useEffect(() => {
+		console.log(state);
+	}, [state]);
 	return (
 		<div>
 			<div className="gameboy__outer__shell">
@@ -90,7 +199,21 @@ function GameBoy() {
 						<div className="gameboy__display__wrapper">
 							<div class="gameboy__display__circle">&nbsp;</div>
 							<div className="gameboy__display__borderwrapper">
-								<div className="gameboy__display">&nbsp;</div>
+								<div className="gameboy__display">
+									&nbsp;
+									<div className="gameboy__display__top"></div>
+									<div className="gameboy__display__bottom">
+										<div className="gameboy__timer">
+											<div className="timer__minutes">
+												{state.showTimer == true ? state.minutes : null}
+											</div>
+											<div className="timer__seconds">
+												{state.showTimer == true ? state.seconds : null}
+											</div>
+										</div>
+										<div className="gameboy__time">{state.time}</div>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
