833bf4945d5253a9d3836d3f361d1646f0f31955
Jordan Molina <jordanmichaelmolina3@gmail.com>
2021-06-21 19:04:16 -0500

    finished window sizing and css stuff

--- src/Components/GameBoy.js
+++ src/Components/GameBoy.js
@@ -523,33 +523,8 @@ function GameBoy({
 		return true;
 	}
 
-	function overlaps(a, b) {
-		const rect1 = a.getBoundingClientRect();
-		const rect2 = b.getBoundingClientRect();
-		const isInHoriztonalBounds =
-			rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x;
-		const isInVerticalBounds =
-			rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y;
-		const isOverlapping = isInHoriztonalBounds && isInVerticalBounds;
-		return isOverlapping;
-	}
+	
 
-	function shuffle(array) {
-		var currentIndex = array.length,
-			randomIndex;
-
-		while (0 !== currentIndex) {
-			randomIndex = Math.floor(Math.random() * currentIndex);
-			currentIndex--;
-
-			[array[currentIndex], array[randomIndex]] = [
-				array[randomIndex],
-				array[currentIndex],
-			];
-		}
-
-		return array;
-	}
 
 	function createNewBullets() {
 		dispatch({ type: "renderNewBullet", value: 0 });
